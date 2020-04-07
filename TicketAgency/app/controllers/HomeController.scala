5527615b7aa6ec1adb981ea42f9c37577b15f8dd
Xiaoge Zhang <zhang.xiaog@northeastern.edu>
2020-04-07 12:54:45 -0400

    purchase ticket

--- TicketAgency/app/controllers/HomeController.scala
+++ TicketAgency/app/controllers/HomeController.scala
@@ -1,20 +1,21 @@
 package controllers
 
-import actors.UserActor
-import actors.UserActor.LoginInfo
+import actors.{EventActor, UserActor}
+import actors.UserActor.{FindByName, LoginInfo, UpdateUser}
 import akka.actor.ActorSystem
 import akka.util.Timeout
 import form.UserData
 import javax.inject._
 import play.api.mvc._
-import pojos.User
+import pojos.{Event, User}
 
 import scala.language.postfixOps
 import scala.concurrent.{Await, Future}
 
 @Singleton
 class  HomeController @Inject()(system: ActorSystem, cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
-    val userActor = system.actorOf(UserActor.props, "userlogin-actor")
+    val userActor = system.actorOf(UserActor.props, "user-actor")
+    val eventActor = system.actorOf(EventActor.props, "event-actor")
 
     import akka.pattern.ask
 
@@ -32,7 +33,7 @@ class  HomeController @Inject()(system: ActorSystem, cc: ControllerComponents) e
     def userPage() = Action { implicit request: Request[AnyContent] =>
         val formData : UserData = UserData.userForm.bindFromRequest().get
         val result: Future[Seq[User]] = (userActor ? LoginInfo(formData.name,formData.password)).mapTo[Seq[User]]
-        val userList = Await.result(result,1000 millis)
+        val userList = Await.result(result,5 seconds)
         if(!userList.isEmpty)
             Ok(views.html.userPage(userList.head)).withSession("test" -> "123123123")
         else
@@ -43,11 +44,22 @@ class  HomeController @Inject()(system: ActorSystem, cc: ControllerComponents) e
         Ok(views.html.userPurchase(name))
     }
 
-    def purchaseResult(user: String, ticket: String) = Action { implicit request: Request[AnyContent] =>
-        val ticketData = ticket.split(",")
-        val event = ticketData.head
+    def purchaseResult(userName: String, ticketInfo: String) = Action { implicit request: Request[AnyContent] =>
+        val ticketData = ticketInfo.split(",")
+        val eventName = ticketData.head
         val ticketType = ticketData.tail.head
-        Ok(views.html.purchaseResult(user, ticket))
+
+        val userResult: Future[Seq[User]] = (userActor ? FindByName(userName)).mapTo[Seq[User]]
+        val user = Await.result(userResult,5 seconds).head
+
+        val eventResult: Future[Seq[Event]] = (eventActor ? FindByName(eventName)).mapTo[Seq[Event]]
+        val event = Await.result(eventResult,30 seconds).head
+
+        val tickets = event.tickets.filter(t => t.ticket_type == ticketType)
+        val newUserTickets = user.tickets :+ tickets.head
+        val newUser = new User(user._id,user.name,user.password, newUserTickets)
+        userActor ? UpdateUser(newUser)
+        Ok(views.html.purchaseResult(userName, ticketInfo))
     }
 
     def owner() = Action { implicit request: Request[AnyContent] =>
